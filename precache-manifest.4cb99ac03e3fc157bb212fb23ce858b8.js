dfad1c3f834a26199d5acffdb6b743db91fdd797
Alex Owl <Sherehime@users.noreply.github.com>
2021-04-07 15:28:00 +0300

    Updates

---
+++ precache-manifest.4cb99ac03e3fc157bb212fb23ce858b8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c57691edbb2f45db6aa826dace932235",
+    "url": "/demo_way_samurai/index.html"
+  },
+  {
+    "revision": "dc1f2d0d38e970360a23",
+    "url": "/demo_way_samurai/static/css/3.ee388f0e.chunk.css"
+  },
+  {
+    "revision": "cce839a6321c146dd305",
+    "url": "/demo_way_samurai/static/css/4.56fe1bf8.chunk.css"
+  },
+  {
+    "revision": "8f32a0f7dbfc28c18a0e",
+    "url": "/demo_way_samurai/static/css/main.9b6fcf15.chunk.css"
+  },
+  {
+    "revision": "31cc674c2d540fa9add2",
+    "url": "/demo_way_samurai/static/js/2.b719dd7e.chunk.js"
+  },
+  {
+    "revision": "dc1f2d0d38e970360a23",
+    "url": "/demo_way_samurai/static/js/3.05dd5eb1.chunk.js"
+  },
+  {
+    "revision": "cce839a6321c146dd305",
+    "url": "/demo_way_samurai/static/js/4.a91150b4.chunk.js"
+  },
+  {
+    "revision": "8f32a0f7dbfc28c18a0e",
+    "url": "/demo_way_samurai/static/js/main.0ad7566c.chunk.js"
+  },
+  {
+    "revision": "2fbef1ae134c5693c98b",
+    "url": "/demo_way_samurai/static/js/runtime~main.936772d7.js"
+  },
+  {
+    "revision": "6c3daf6085e46eea1698990ad3578119",
+    "url": "/demo_way_samurai/static/media/preloader.6c3daf60.svg"
+  },
+  {
+    "revision": "8b1dbb1f4e4468b9cc31ced9c63fcfe7",
+    "url": "/demo_way_samurai/static/media/user.8b1dbb1f.png"
+  }
+]);
